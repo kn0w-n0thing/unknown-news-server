ffc36cb9e576dbde2b2f15c7b3a6324933384df3
clldaxigua <clldaxigua@126.com>
2023-04-07 14:35:42 +0800

    Handle gui request for getting news data.

--- src/main/java/org/chronusartcenter/Main.java
+++ src/main/java/org/chronusartcenter/Main.java
@@ -1,17 +1,11 @@
 package org.chronusartcenter;
 
-import com.alibaba.fastjson2.JSONObject;
-import org.chronusartcenter.dalle.DalleService;
-import org.chronusartcenter.news.NewsService;
-
 public class Main {
     public static void main(String[] args) {
         Context context = new Context();
         try {
-//            Server server = new Server();
-//            server.start();
-            DalleService dalleService = new DalleService();
-            System.out.println(dalleService.generateImage("Donald Trump loves China", 1));
+            SocketServer server = new SocketServer(context);
+            server.start();
         } catch (Exception e) {
             // TODO: log
             e.printStackTrace();
---
+++ src/main/java/org/chronusartcenter/SocketServer.java
@@ -0,0 +1,93 @@
+package org.chronusartcenter;
+
+import io.socket.socketio.server.SocketIoNamespace;
+import io.socket.socketio.server.SocketIoServer;
+import io.socket.socketio.server.SocketIoSocket;
+import org.chronusartcenter.dalle.DalleService;
+import org.chronusartcenter.network.ServerWrapper;
+import org.chronusartcenter.news.NewsService;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+
+public class SocketServer {
+    private final Context context;
+    private final AtomicBoolean isProcessing = new AtomicBoolean(false);
+
+    private static final String GUI_LOG_EVENT = "gui-log";
+    private static final String GET_NEWS_DATA_EVENT = "get-news-data";
+
+    public SocketServer(Context context) {
+        this.context = context;
+    }
+
+    private void listenEvents(SocketIoSocket socket) {
+        if (socket == null) {
+            return;
+        }
+
+        socket.on("disconnect", args -> {
+            // TODO: log
+            System.out.println("Client " + socket.getId() + " (" + socket.getInitialHeaders().get("remote_addr") + ") has disconnected.");
+            System.out.println("Reason: " + args[0]);
+        });
+
+        socket.on(GET_NEWS_DATA_EVENT, args -> {
+            System.out.println("get-news-data, isProcessing: " + isProcessing.get());
+            if (isProcessing.get()) {
+                System.out.println("Ignore request");
+                socket.send(GET_NEWS_DATA_EVENT, false, "Processing, please wait...");
+                return;
+            }
+
+            isProcessing.set(true);
+            socket.send(GUI_LOG_EVENT, "Start to process.");
+            System.out.println("on get-news-data");
+            NewsService newsService = new NewsService(context);
+            var news = newsService.translateHeadlines(newsService.fetchHeadlines());
+            System.out.println(news);
+            // make dalle service singleton
+            DalleService dalleService = new DalleService();
+            news.stream().forEach(headlineModel -> {
+                var image = dalleService.generateImage(headlineModel.getTranslation(), 1);
+                System.out.println(image.left);
+            });
+            // TODO: test code
+//            news.subList(0,1).stream().forEach(headlineModel -> {
+//                var image = dalleService.generateImage(headlineModel.getTranslation(), 1);
+//                System.out.println(image.left);
+//            });
+            socket.send(GET_NEWS_DATA_EVENT, true, "Completed.");
+            isProcessing.set(false);
+        });
+
+        socket.on("get-local-data", arg -> {});
+        socket.on("save-config", arg -> {});
+        socket.on("dalle-status", arg -> {
+            // TODO.log
+            System.out.println("On dalle-status");
+            DalleService dalleService = new DalleService();
+            socket.send("dalle-status", dalleService.check());
+        });
+        socket.on("shutdown", arg -> {});
+    }
+
+    public void start() {
+        // TODO: read port from config file
+        final ServerWrapper serverWrapper = new ServerWrapper("127.0.0.1", 4000, null);
+        try {
+            serverWrapper.startServer();
+        } catch (Exception e) {
+            // TODO: log
+            e.printStackTrace();
+        }
+        SocketIoServer server = serverWrapper.getSocketIoServer();
+        SocketIoNamespace namespace = server.namespace("/");
+
+        namespace.on("connection", args -> {
+            SocketIoSocket socket = (SocketIoSocket) args[0];
+            // TODO: log
+            System.out.println("Client " + socket.getId() + " (" + socket.getInitialHeaders().get("remote_addr") + ") has connected.");
+            listenEvents(socket);
+        });
+    }
+}
